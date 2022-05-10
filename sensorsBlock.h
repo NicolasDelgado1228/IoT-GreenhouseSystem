bc27869a942768e4eb5b75fc44115e4d980df730
paladinescamila <camilapaladines27@gmail.com>
2022-05-10 18:11:47 -0500

    Added sensors block signals and states

--- sensorsBlock.h
+++ sensorsBlock.h
@@ -7,3 +7,56 @@
 *   Notes:          User defined according to application
 *
 *******************************************************************************/
+
+// SIGNALS
+
+typedef enum {
+    sGetData,
+    sSendTemp,
+    sSendHumidity,
+    sSendPh,
+    sSendCO2
+} TO_CONTROLLER;
+
+typedef enum {
+    sGetTemp
+} TO_THERMOMETER;
+
+typedef enum {
+    sGetHumidity
+} TO_HUMIDITY_SENSOR;
+
+typedef enum {
+    sGetPh
+} TO_PH_SENSOR;
+
+typedef enum {
+    sGetCO2
+} TO_CO2_SENSOR;
+
+
+// STATES
+
+typedef enum {
+    IdleController,
+    WaitingTemp,
+    WaitingHumidity,
+    WaitingPh,
+    WaitingCO2
+} CONTROLLER_STATES;
+
+typedef enum {
+    IdleTemp,
+} THERMOMETER_STATES;
+
+typedef enum {
+    IdleHum,
+} HUMIDITY_STATES;
+
+typedef enum {
+    IdlePh,
+} PH_STATES;
+
+typedef enum {
+    IdleCO2,
+} CO2_STATES;
