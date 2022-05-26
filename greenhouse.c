4b6c58b4471ad6862e0537f96a3d62f8334f4a5b
Camila Paladines <camilapaladines27@gmail.com>
2022-05-26 00:59:30 -0500

    Added scaled-out version

--- greenhouse.c
+++ greenhouse.c
@@ -15,6 +15,14 @@
 #include <time.h>
 #include "phtrdsMsgLyr.h"
 
+/*  MODE
+    0: User -> displays the results table
+    1: Debug -> displays message sending
+    2: User and debug -> combines user and debug modes
+*/
+
+#define MODE 0
+
 // Function to generate a random number in a defined range
 float randomFloat (float min, float max){
     return (((double)rand()) / ((double)RAND_MAX)) * (max - min) + min;
@@ -33,11 +41,6 @@ static void *pTimer(void *arg);
 int main (void) {
 
     pthread_t console_tid;
-    pthread_t controller_tid;
-    pthread_t thermometer_tid;
-    pthread_t humidity_sensor_tid;
-    pthread_t ph_sensor_tid;
-    pthread_t co2_sensor_tid;
     pthread_t timer_tid;
 
     // Create queues
@@ -45,20 +48,10 @@ int main (void) {
 
     // Create threads
     pthread_create(&console_tid, NULL, pConsole, NULL);
-    pthread_create(&controller_tid, NULL, pController, NULL);
-    pthread_create(&thermometer_tid, NULL, pThermometer, NULL);
-    pthread_create(&humidity_sensor_tid, NULL, pHumiditySensor, NULL);
-    pthread_create(&ph_sensor_tid, NULL, pPhSensor, NULL);
-    pthread_create(&co2_sensor_tid, NULL, pCO2Sensor, NULL);
     pthread_create(&timer_tid, NULL, pTimer, NULL);
 
     // Wait for threads to finish
     pthread_join(console_tid, NULL);
-    pthread_join(controller_tid, NULL);
-    pthread_join(thermometer_tid, NULL);
-    pthread_join(humidity_sensor_tid, NULL);
-    pthread_join(ph_sensor_tid, NULL);
-    pthread_join(co2_sensor_tid, NULL);
     pthread_join(timer_tid, NULL);
 
     // Destroy queues
@@ -72,8 +65,30 @@ static void *pConsole (void *arg) {
     CONSOLE_STATES state, next_state;
     msg_t InMsg, OutMsg;
     float Temp, Hum, pH, CO2;
-    int scanFrecuency;
+
+    int scanFrecuency, i;
     char line[100];
+
+    time_t t;
+    struct tm localTime;
+    char dateTime[70];
+    char *format = "%d-%m-%Y %H:%M:%S";
+
+    pthread_t controllers[N_PLANTS];
+    pthread_t thermometers[N_PLANTS];
+    pthread_t humiditySensors[N_PLANTS];
+    pthread_t phSensors[N_PLANTS];
+    pthread_t co2Sensors[N_PLANTS];
+
+    // Create threads
+    for (i = 0; i < N_PLANTS; i++) {
+        pthread_create(&controllers[i], NULL, pController, (void *) &i);
+        pthread_create(&thermometers[i], NULL, pThermometer, (void *) &i);
+        pthread_create(&humiditySensors[i], NULL, pHumiditySensor, (void *) &i);
+        pthread_create(&phSensors[i], NULL, pPhSensor, (void *) &i);
+        pthread_create(&co2Sensors[i], NULL, pCO2Sensor, (void *) &i);
+        sleep(1);
+    }
 
     next_state = IdleConsole;
 
@@ -91,56 +106,72 @@ static void *pConsole (void *arg) {
         switch (state) {
             case IdleConsole:
                 // Send signal to obtain data
-                OutMsg.signal = (int) sGetData;
-                printf("Enviando mensaje sGetData desde pConsole a pController...\n");
-                sendMessage(&(queue[CONTROLLER_Q]), OutMsg);
-                next_state = WaitingController;
+                for (i = 0; i < N_PLANTS; i++){
+                    OutMsg.signal = (int) sGetData;
+                    if (MODE == 1 || MODE == 2) printf("Enviando mensaje sGetData desde pConsole a pController (ID = %d)...\n", i);
+                    sendMessage(&(queue[2 + 6*i + CONTROLLER_Q]), OutMsg);
+                    next_state = WaitingController;
+                }
                 break;
             
             case WaitingController:
-                // Receives data from the controller
-                InMsg = receiveMessage(&(queue[CONSOLE_Q]));
-                switch (InMsg.signal) {
-                    case sSendData:
 
-                        // Saves the obtained data
-                        Temp = InMsg.value;
-                        Hum = InMsg.value2;
-                        pH = InMsg.value3;
-                        CO2 = InMsg.value4;
+                // Gets the current date and time
+                t = time(NULL);
+                localTime = *localtime(&t);
+                strftime(dateTime, sizeof dateTime, format, &localTime);
 
-                        // Gets the current date and time
-                        time_t t = time(NULL);
-                        struct tm localTime = *localtime(&t);
-                        char dateTime[70];
-                        char *format = "%Y-%m-%d %H:%M:%S";
-                        strftime(dateTime, sizeof dateTime, format, &localTime);
+                // Displays data to the user
+                if (MODE == 0 || MODE == 2){
+                    printf("\n\n");
+                    printf("ESCANEO DE VARIABLES (%s)\n", dateTime);
+                    printf("------------------------------------------------------------------\n");
+                    printf("Planta \tTemperatura\tHumedad\t\tNivel pH\tNivel CO2\n");
+                    printf("------------------------------------------------------------------\n");
+                }
 
-                        // Displays data to the user
-                        printf("ESCANEO DE VARIABLES (%s)\n", dateTime);
-                        printf("- Temperatura: %.1f °C\n- Humedad: %.1f %%\n- pH: %.1f\n- CO2: %.1f ppm\n", Temp, Hum, pH, CO2);
-                        printf("-----------------------------------------\n");
+                for (i = 0; i < N_PLANTS; i++){
+                    // Receives data from the controller
+                    InMsg = receiveMessage(&(queue[2 + 6*i + CONSOLE_Q]));
+                    switch (InMsg.signal) {
+                        case sSendData:
 
-                        // Sends a signal to the timer to count the waiting time
-                        OutMsg.signal = (int) sSetTimer;
-                        OutMsg.value = scanFrecuency;
-                        printf("Enviando mensaje sSetTimer desde pConsole a pTimer...\n");
-                        sendMessage(&(queue[TIMER_Q]), OutMsg);
-                        next_state = WaitingTimer;
-                        break;
-                    default:
-                        break;
+                            // Saves the obtained data
+                            Temp = InMsg.value;
+                            Hum = InMsg.value2;
+                            pH = InMsg.value3;
+                            CO2 = InMsg.value4;
+
+                            // Displays data to the user
+                            if (MODE == 0 || MODE == 2) printf("%d \t%.1f °C\t\t%.1f %%\t\t%.1f\t\t%.1f ppm\n", InMsg.sender, Temp, Hum, pH, CO2);
+
+                            if (i + 1 == N_PLANTS) {
+                                // Sends a signal to the timer to count the waiting time
+                                OutMsg.signal = (int) sSetTimer;
+                                OutMsg.value = scanFrecuency;
+                                if (MODE == 1 || MODE == 2) printf("Enviando mensaje sSetTimer desde pConsole a pTimer...\n");
+                                sendMessage(&(queue[TIMER_Q]), OutMsg);
+                                next_state = WaitingTimer;
+                            }
+                            break;
+                        default:
+                            break;
+                    }
                 }
                 break;
 
             case WaitingTimer:
                 // Receives signal from the timer and can continue scanning
-                InMsg = receiveMessage(&(queue[CONSOLE_Q]));
+                InMsg = receiveMessage(&(queue[TIMER_TO_CONSOLE_Q]));
                 switch (InMsg.signal){
                     case sTimeout:
-                        OutMsg.signal = (int) sGetData;
-                        printf("Enviando mensaje sGetData desde pConsole a pController...\n");
-                        sendMessage(&(queue[CONTROLLER_Q]), OutMsg);
+
+                        for (i = 0; i < N_PLANTS; i++) {
+                            OutMsg.signal = (int) sGetData;
+                            if (MODE == 1 || MODE == 2) printf("Enviando mensaje sGetData desde pConsole a pController (ID = %d)...\n", i);
+                            sendMessage(&(queue[2 + 6*i + CONTROLLER_Q]), OutMsg);
+                        }
+
                         next_state = WaitingController;
                         break;
                 }
@@ -151,11 +182,23 @@ static void *pConsole (void *arg) {
         }
     }
 
+    // Wait for threads to finish
+    for (i = 0; i < N_PLANTS; i++){
+        pthread_join(controllers[i], NULL);
+        pthread_join(thermometers[i], NULL);
+        pthread_join(humiditySensors[i], NULL);
+        pthread_join(phSensors[i], NULL);
+        pthread_join(co2Sensors[i], NULL);
+    }
+
     return NULL;
 }
 
 // Controller thread
 static void *pController (void *arg) {
+    int *data = (int *) arg;
+    int ID = *data;
+
     CONTROLLER_STATES state, next_state;
     msg_t InMsg, OutMsg;
 
@@ -165,7 +208,7 @@ static void *pController (void *arg) {
     for ( ; ; ) {
 
         state = next_state;
-        InMsg = receiveMessage(&(queue[CONTROLLER_Q]));
+        InMsg = receiveMessage(&(queue[2 + 6*ID + CONTROLLER_Q]));
 
         switch (state) {
 
@@ -174,8 +217,8 @@ static void *pController (void *arg) {
                     case sGetData:
                         // Request temperature
                         OutMsg.signal = (int) sGetTemp;
-                        printf("Enviando mensaje sGetTemp desde pController a pThermometer...\n");
-                        sendMessage(&(queue[THERMOMETER_Q]), OutMsg);
+                        if (MODE == 1 || MODE == 2) printf("Enviando mensaje sGetTemp desde pController (ID = %d) a pThermometer (ID = %d)...\n", ID, ID);
+                        sendMessage(&(queue[2 + 6*ID + THERMOMETER_Q]), OutMsg);
                         next_state = WaitingTemp;
                         break;
                     default:
@@ -189,8 +232,8 @@ static void *pController (void *arg) {
                         // Receives temperature and requests humidity
                         sensorsArray[0] = InMsg.value;
                         OutMsg.signal = (int) sGetHumidity;
-                        printf("Enviando mensaje sGetHumidity desde pController a pHumiditySensor...\n");
-                        sendMessage(&(queue[HUM_SENSOR_Q]), OutMsg);
+                        if (MODE == 1 || MODE == 2) printf("Enviando mensaje sGetHumidity desde pController (ID = %d) a pHumiditySensor (ID = %d)...\n", ID, ID);
+                        sendMessage(&(queue[2 + 6*ID + HUM_SENSOR_Q]), OutMsg);
                         next_state = WaitingHumidity;
                         break;
                     default:
@@ -204,8 +247,8 @@ static void *pController (void *arg) {
                         // Receives humidity and requests pH
                         sensorsArray[1] = InMsg.value;
                         OutMsg.signal = (int) sGetPh;
-                        printf("Enviando mensaje sGetPh desde pController a pPhSensor...\n");
-                        sendMessage(&(queue[PH_SENSOR_Q]), OutMsg);
+                        if (MODE == 1 || MODE == 2) printf("Enviando mensaje sGetPh desde pController (ID = %d) a pPhSensor (ID = %d)...\n", ID, ID);
+                        sendMessage(&(queue[2 + 6*ID + PH_SENSOR_Q]), OutMsg);
                         next_state = WaitingPh;
                         break;
                     default:
@@ -219,8 +262,8 @@ static void *pController (void *arg) {
                         // Receives pH and requests CO2
                         sensorsArray[2] = InMsg.value;
                         OutMsg.signal = (int) sGetCO2;
-                        printf("Enviando mensaje sGetCO2 desde pController a pCO2Sensor...\n");
-                        sendMessage(&(queue[CO2_SENSOR_Q]), OutMsg);
+                        if (MODE == 1 || MODE == 2) printf("Enviando mensaje sGetCO2 desde pController (ID = %d) a pCO2Sensor (ID = %d)...\n", ID, ID);
+                        sendMessage(&(queue[2 + 6*ID + CO2_SENSOR_Q]), OutMsg);
                         next_state = WaitingCO2;
                         break;
                     default:
@@ -234,12 +277,13 @@ static void *pController (void *arg) {
                         // Receives CO2 and sends the four variables to the console
                         sensorsArray[3] = InMsg.value;
                         OutMsg.signal = (int) sSendData;
+                        OutMsg.sender = (int) ID;
                         OutMsg.value = sensorsArray[0];
                         OutMsg.value2 = sensorsArray[1];
                         OutMsg.value3 = sensorsArray[2];
                         OutMsg.value4 = sensorsArray[3];
-                        printf("Enviando mensaje sSendData desde pController a pConsole con los valores %.1f, %.1f, %.1f y %.1f...\n", OutMsg.value, OutMsg.value2, OutMsg.value3, OutMsg.value4);
-                        sendMessage(&(queue[CONSOLE_Q]), OutMsg);
+                        if (MODE == 1 || MODE == 2) printf("Enviando mensaje sSendData desde pController (ID = %d) a pConsole con los valores %.1f, %.1f, %.1f y %.1f...\n", ID, OutMsg.value, OutMsg.value2, OutMsg.value3, OutMsg.value4);
+                        sendMessage(&(queue[2 + 6*ID + CONSOLE_Q]), OutMsg);
                         next_state = IdleController;
                         break;
                     default:
@@ -257,6 +301,9 @@ static void *pController (void *arg) {
 
 // Thermometer thread
 static void *pThermometer (void *arg) {
+    int *data = (int *) arg;
+    int ID = *data;
+
     THERMOMETER_STATES state, next_state;
     msg_t InMsg, OutMsg;
     float Temp;
@@ -266,7 +313,7 @@ static void *pThermometer (void *arg) {
     for ( ; ; ) {
 
         state = next_state;
-        InMsg = receiveMessage(&(queue[THERMOMETER_Q]));
+        InMsg = receiveMessage(&(queue[2 + 6*ID + THERMOMETER_Q]));
         Temp = randomFloat(25, 27);
 
         switch (state) {
@@ -275,8 +322,8 @@ static void *pThermometer (void *arg) {
                     case sGetTemp:
                         OutMsg.signal = (int) sSendTemp;
                         OutMsg.value = Temp;
-                        printf("Enviando mensaje sSendTemp desde pThermometer a pController con el valor %.1f...\n", Temp);
-                        sendMessage(&(queue[CONTROLLER_Q]), OutMsg);
+                        if (MODE == 1 || MODE == 2) printf("Enviando mensaje sSendTemp desde pThermometer (ID = %d) a pController (ID = %d) con el valor %.1f...\n", ID, ID, Temp);
+                        sendMessage(&(queue[2 + 6*ID + CONTROLLER_Q]), OutMsg);
                         next_state = IdleTemp;
                         break;
                     default:
@@ -293,6 +340,9 @@ static void *pThermometer (void *arg) {
 
 // Humidity sensor thread
 static void *pHumiditySensor (void *arg) {
+    int *data = (int *) arg;
+    int ID = *data;
+
     HUMIDITY_STATES state, next_state;
     msg_t InMsg, OutMsg;
     float Hum;
@@ -302,7 +352,7 @@ static void *pHumiditySensor (void *arg) {
     for ( ; ; ) {
 
         state = next_state;
-        InMsg = receiveMessage(&(queue[HUM_SENSOR_Q]));
+        InMsg = receiveMessage(&(queue[2 + 6*ID + HUM_SENSOR_Q]));
         Hum = randomFloat(85, 87);
 
         switch (state) {
@@ -311,8 +361,8 @@ static void *pHumiditySensor (void *arg) {
                     case sGetHumidity:
                         OutMsg.signal = (int) sSendHumidity;
                         OutMsg.value = Hum;
-                        printf("Enviando mensaje sSendHumidity desde pHumiditySensor a pController con el valor %.1f...\n", Hum);
-                        sendMessage(&(queue[CONTROLLER_Q]), OutMsg);
+                        if (MODE == 1 || MODE == 2) printf("Enviando mensaje sSendHumidity desde pHumiditySensor (ID = %d) a pController (ID = %d) con el valor %.1f...\n", ID, ID, Hum);
+                        sendMessage(&(queue[2 + 6*ID + CONTROLLER_Q]), OutMsg);
                         next_state = IdleHum;
                         break;
                     default:
@@ -329,6 +379,9 @@ static void *pHumiditySensor (void *arg) {
 
 // pH sensor thread
 static void *pPhSensor (void *arg) {
+    int *data = (int *) arg;
+    int ID = *data;
+
     PH_STATES state, next_state;
     msg_t InMsg, OutMsg;
     float pH;
@@ -338,7 +391,7 @@ static void *pPhSensor (void *arg) {
     for ( ; ; ) {
 
         state = next_state;
-        InMsg = receiveMessage(&(queue[PH_SENSOR_Q]));
+        InMsg = receiveMessage(&(queue[2 + 6*ID + PH_SENSOR_Q]));
         pH = randomFloat(6.3, 6.7);
 
         switch (state) {
@@ -347,8 +400,8 @@ static void *pPhSensor (void *arg) {
                     case sGetPh:
                         OutMsg.signal = (int) sSendPh;
                         OutMsg.value = pH;
-                        printf("Enviando mensaje sSendPh desde pPhSensor a pController con el valor %.1f...\n", pH);
-                        sendMessage(&(queue[CONTROLLER_Q]), OutMsg);
+                        if (MODE == 1 || MODE == 2) printf("Enviando mensaje sSendPh desde pPhSensor (ID = %d) a pController (ID = %d) con el valor %.1f...\n", ID, ID, pH);
+                        sendMessage(&(queue[2 + 6*ID + CONTROLLER_Q]), OutMsg);
                         next_state = IdlePh;
                         break;
                     default:
@@ -365,6 +418,9 @@ static void *pPhSensor (void *arg) {
 
 // CO2 sensor thread
 static void *pCO2Sensor (void *arg) {
+    int *data = (int *) arg;
+    int ID = *data;
+
     CO2_STATES state, next_state;
     msg_t InMsg, OutMsg;
     float CO2;
@@ -374,7 +430,7 @@ static void *pCO2Sensor (void *arg) {
     for ( ; ; ) {
 
         state = next_state;
-        InMsg = receiveMessage(&(queue[CO2_SENSOR_Q]));
+        InMsg = receiveMessage(&(queue[2 + 6*ID + CO2_SENSOR_Q]));
         CO2 = randomFloat(1150, 1250);
 
         switch (state) {
@@ -383,8 +439,8 @@ static void *pCO2Sensor (void *arg) {
                     case sGetCO2:
                         OutMsg.signal = (int) sSendCO2;
                         OutMsg.value = CO2;
-                        printf("Enviando mensaje sSendCO2 desde pCO2Sensor a pController con el valor %.1f...\n", CO2);
-                        sendMessage(&(queue[CONTROLLER_Q]), OutMsg);
+                        if (MODE == 1 || MODE == 2) printf("Enviando mensaje sSendCO2 desde pCO2Sensor (ID = %d) a pController (ID = %d) con el valor %.1f...\n", ID, ID, CO2);
+                        sendMessage(&(queue[2 + 6*ID + CONTROLLER_Q]), OutMsg);
                         next_state = IdleCO2;
                         break;
                     default:
@@ -419,7 +475,7 @@ static void *pTimer (void *arg) {
                         sleep(InMsg.value);
                         OutMsg.signal = (int) sTimeExpired;
                         OutMsg.value = 0;
-                        printf("Enviando mensaje sTimeExpired desde pTimer a sí mismo con el valor 0...\n");
+                        if (MODE == 1 || MODE == 2) printf("Enviando mensaje sTimeExpired desde pTimer a sí mismo con el valor 0...\n");
                         sendMessage(&(queue[TIMER_Q]), OutMsg);
                         next_state = CheckTimeout;
                         break;
@@ -433,8 +489,8 @@ static void *pTimer (void *arg) {
                     case sTimeExpired:
                         OutMsg.signal = (int) sTimeout;
                         OutMsg.value = 0;
-                        printf("Enviando mensaje sTimeout desde pTimer a pConsole con el valor 0...\n");
-                        sendMessage(&(queue[CONSOLE_Q]), OutMsg);
+                        if (MODE == 1 || MODE == 2) printf("Enviando mensaje sTimeout desde pTimer a pConsole con el valor 0...\n");
+                        sendMessage(&(queue[TIMER_TO_CONSOLE_Q]), OutMsg);
                         next_state = IdleTimer;
                         break;
                     case sResetTimer:
--- greenhouse.h
+++ greenhouse.h
@@ -10,15 +10,17 @@
 #include "consoleBlock.h"
 #include "sensorsBlock.h"
 
+#define N_PLANTS 3
 #define BUFSIZE 8
-#define NUM_QUEUES 7
+#define NUM_QUEUES 2 + N_PLANTS*6
+#define TIMER_Q 0
+#define TIMER_TO_CONSOLE_Q 1
 #define CONSOLE_Q 0
 #define CONTROLLER_Q 1
 #define THERMOMETER_Q 2
 #define HUM_SENSOR_Q 3
 #define PH_SENSOR_Q 4
 #define CO2_SENSOR_Q 5
-#define TIMER_Q 6
 
 typedef struct {
     int signal;
@@ -26,4 +28,5 @@ typedef struct {
     float value2;
     float value3;
     float value4;
+    int sender;
 } msg_t;
